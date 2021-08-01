ef5bda484f6d5b9c05c1328c9b13727a429144c2
saranyamaity <saranyamaity2000@gmail.com>
2021-08-01 17:25:07 +0530

    portion of Palindromic Partioning

---
+++ PalindromDP.cpp
@@ -0,0 +1,13 @@
+for(gap = 0; gap < n; gap++){
+            for(i = 0 ; i < n - gap ; i++){
+                j = i + gap;
+                if(gap == 0)
+                    P[i][j] = 1; 
+                else if(gap == 1)
+                    P[i][j] = (str[i] == str[j]);
+                else
+                    P[i][j] = ((str[i] == str[j]) & P[i+1][j-1]);
+            } 
+}
+
+// P[i][j] stores if(i to j) portion is palindrom or not ! 
