7d0ba1937432caa102deefd6721b5e87c3dfa3e1
saranyamaity <saranyamaity2000@gmail.com>
2022-03-25 12:08:17 +0530

    new DP palindrome insertion prpblem added

---
+++ Palindrom_Min_Insertion_DP.cpp
@@ -0,0 +1,20 @@
+int findMinInsertions(string s){
+        int n = s.size() ;
+        int dp[n][n] ; 
+        memset(dp , 0 , sizeof(dp)) ; 
+        for(int gap = 0; gap<n ; gap++){
+            for(int i =0 ; i<n- gap ; i++){
+                int j = i + gap ; 
+                if(gap == 0) dp[i][j] = 0 ; 
+                else if(gap == 1) dp[i][j] = s[i] != s[j] ; 
+                else {
+                    if(s[i] == s[j]){
+                        dp[i][j] = dp[i+1][j-1] ; 
+                    }else{
+                        dp[i][j] = 1 + min(dp[i+1][j] , dp[i][j-1]) ; 
+                    }
+                }
+            }
+        }
+        return dp[0][n-1] ; 
+    }
